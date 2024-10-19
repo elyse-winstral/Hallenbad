f6d17295ebff7705bbf91f22aaa25b6d77168e2f
Elyse Winstral <elyseeemw@gmail.com>
2024-10-19 08:59:18 +0200

    First commit

---
+++ webscraping_hallenbad.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from datetime import datetime 
+import time
+import pandas as pd
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(5)
+
+URL = "https://www.stadt-zuerich.ch/ssd/de/index/sport/schwimmen/hallenbaeder/hallenbad_oerlikon.html"
+driver.get(URL)
+
+nums = driver.find_element(By.ID, "SSD-7")
+occupancy = nums.text
+occupancy = int(occupancy)
+driver.quit()
+
+
+header = ["Time", "Occupancy"]
+
+#UPDATE TIME PORTION OF CODE FOR STRF
+date = str(time.localtime().tm_mday) + "/" + str(time.localtime().tm_mon) + "-" + str(time.localtime().tm_hour) + ":" + str(time.localtime().tm_min)
+data = [date, occupancy]
+
+df = pd.read_csv("results.csv")
+df = pd.concat([df, pd.DataFrame([data], columns = header)], ignore_index=True)
+df.to_csv("results.csv",index=False)
